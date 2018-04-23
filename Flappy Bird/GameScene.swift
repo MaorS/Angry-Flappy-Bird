9fb24cc7837061e0e8dc5a27b508874e054333d8
Maor Shamsian <maor.shams@gmail.com>
2018-04-23 12:58:51 +0300

    Walls physics + bird fly on touch

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -28,13 +28,21 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        // on bird touch
+        bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60))
+        
+        
     }
+    
     private func addGround(){
         ground = SKSpriteNode(imageNamed: "image_ground")
         ground.setScale(0.5)
         let groundX = self.frame.width / 2
         let groundY = ground.frame.height / 2
         ground.position = CGPoint(x: groundX, y: groundY)
+        
+        // Physics Body:
         ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
         ground.physicsBody?.categoryBitMask = Physics.ground
         ground.physicsBody?.collisionBitMask = Physics.bird
@@ -43,6 +51,8 @@ class GameScene: SKScene {
         
         // unmoving on hit
         ground.physicsBody?.isDynamic = false
+        
+        ground.zPosition = 3
         self.addChild(ground)
     }
     
@@ -52,14 +62,17 @@ class GameScene: SKScene {
         let birdX = self.frame.width / 2 - bird.frame.width
         let birdY = self.frame.height / 2
         bird.position = CGPoint(x: birdX, y: birdY)
+        
+        // Physics Body:
         bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height/2)
         bird.physicsBody?.categoryBitMask = Physics.bird
         bird.physicsBody?.collisionBitMask = Physics.ground | Physics.wall
         bird.physicsBody?.contactTestBitMask = Physics.ground | Physics.wall
-        
         bird.physicsBody?.affectedByGravity = true
         // effect by hit
         bird.physicsBody?.isDynamic = true
+        
+        bird.zPosition = 2
         self.addChild(bird)
         
     }
@@ -69,15 +82,34 @@ class GameScene: SKScene {
         let topWall = SKSpriteNode(imageNamed: "image_wall")
         let bottomWall = SKSpriteNode(imageNamed: "image_wall")
         
-        topWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + 350)
-        
-        bottomWall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 - 350)
+        topWall.position = CGPoint(x: self.frame.width, y: self.frame.height / 2 + 350)
+
+        bottomWall.position = CGPoint(x: self.frame.width, y: self.frame.height / 2 - 350)
         
         topWall.setScale(0.5)
         bottomWall.setScale(0.5)
         
+        // Physics Body:
+
+        topWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
+        topWall.physicsBody?.categoryBitMask = Physics.wall
+        topWall.physicsBody?.collisionBitMask = Physics.bird
+        topWall.physicsBody?.contactTestBitMask = Physics.bird
+        topWall.physicsBody?.isDynamic = false
+        topWall.physicsBody?.affectedByGravity = false
+        
+        bottomWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
+        bottomWall.physicsBody?.categoryBitMask = Physics.wall
+        bottomWall.physicsBody?.collisionBitMask = Physics.bird
+        bottomWall.physicsBody?.contactTestBitMask = Physics.bird
+        bottomWall.physicsBody?.isDynamic = false
+        bottomWall.physicsBody?.affectedByGravity = false
+        
+        
         walls.addChild(topWall)
         walls.addChild(bottomWall)
+        
+        walls.zPosition = 1
         
         self.addChild(walls)
         
